3fcb21a561a80fd477eae5f35b59fbb74a094c9f
Егор <job.ea@icloud.com>
2022-07-06 23:57:46 +0500

    model's changes

--- src/common/model/Model.js
+++ src/common/model/Model.js
@@ -1,8 +1,37 @@
+import { makeObservable, action, computed, observable } from "mobx";
+
 class Model {
+    props = ['id'];
     model = null;
 
     constructor(newModel) {
         this.model = newModel;
+
+        makeObservable(this, {
+            props: observable,
+            model: observable,
+            setProps: action,
+            setModel: action,
+            getModel: computed
+        });
+    }
+
+    _doesModelEnsurePropsCorrect(newModel) {
+        // TODO: написать проверку на свойства.
+        // написать проверку на null и undefined.
+        // проверить на кол-во свойств. Object.keys(newModel) - возвращает ключи в виде массива.
+        // проверить на вхождение всех свойств. props.includes() если хоть одно не входит в массив, выбросить исключение.
+        // при не прохождении проверок выбрасывать исключения.
+        // throw new Error('описание').
+    }
+
+    _getIncorrectModelProps(newModel) {
+        // TODO: взять некорректные свойства модели.
+    }
+
+    setProps(newProps = ['id']) {
+        // если в props нет свойства id, выбросить исключение.
+        // заменить текущий props новым.
     }
 
     setModel(newModelRest) {
